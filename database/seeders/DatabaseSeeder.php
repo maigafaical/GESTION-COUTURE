b4739a4f5391d90e57b29a7a2b7905490f057f66
MAIGA FAICAL <faicalmaiga78@gmail.com>
2023-09-01 11:55:58 +0000

    ajout des seeders par FAICAL

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,22 +1,35 @@
 <?php
-
+ 
 namespace Database\Seeders;
-
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+ 
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+ 
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Run the database seeders.
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            'name' => 'MAIGA FAICAL',
+            'email' => 'faicalmaiga78@gmail.com',
+            'password' => Hash::make('Faical54@'),
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-    }
+        DB::table('users')->insert([
+            'name' => 'DAOUDA ULRICH',
+            'email' => 'ulrichdaouda@gmail.com',
+            'password' => Hash::make('ulrich1236'),
+        ]);
+
+        DB::table('users')->insert([
+            'name' => 'BEMSSE FLORA',
+            'email' => 'fsislinebemsse@gmail.com',
+            'password' => Hash::make('00000000'),
+        ]);
+   }
 }
